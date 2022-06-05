95bc952a493067fc917da90b665e307efa6f0529
umaranis <mail@umaranis.com>
2022-06-05 19:33:48 +1000

    image insert working for svelte-kit project

--- demos/sveltekit/src/routes/index.svelte
+++ demos/sveltekit/src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script>
-    import { RichTextBasicComposer } from 'svelte-lexical';
+    import { RichTextComposer } from 'svelte-lexical';
     import PlaygroundEditorTheme from '../themes/PlaygroundEditorTheme';
     import '../global.css';
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<RichTextBasicComposer theme={PlaygroundEditorTheme}/>
+<RichTextComposer theme={PlaygroundEditorTheme}/>
