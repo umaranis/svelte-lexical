ff78528925ef11ed9edd7531ac050d50c4b3b5ec
umaranis <mail@umaranis.com>
2023-01-06 16:49:49 +1100

    feat: delete selected HorizontalRule on pressing delete or backspace key

--- packages/svelte-lexical/src/core/plugins/HorizontalRuleComponent.svelte
+++ packages/svelte-lexical/src/core/plugins/HorizontalRuleComponent.svelte
@@ -1,35 +1,67 @@
 <script lang="ts">
-  import { mergeRegister } from "@lexical/utils";
-  import { CLICK_COMMAND, COMMAND_PRIORITY_LOW, type LexicalEditor } from "lexical";
-  import { getContext, onMount } from "svelte";
+  import {mergeRegister} from "@lexical/utils";
   import {
-    clearEditorSelection,
-    isNodeSelected,
-    setEditorSelection,
-  } from "../lexicalNodeSelection";
+    $getNodeByKey as getNodeByKey,
+    $getSelection as getSelection,
+    $isNodeSelection as isNodeSelection,
+    CLICK_COMMAND,
+    COMMAND_PRIORITY_LOW,
+    KEY_BACKSPACE_COMMAND,
+    KEY_DELETE_COMMAND,
+    type LexicalEditor,
+  } from "lexical";
+  import {onMount} from "svelte";
+  import {clearEditorSelection, isNodeSelected, setEditorSelection,} from "../lexicalNodeSelection";
+  import {$isHorizontalRuleNode as isHorizontalRuleNode} from "./HorizontalRuleNode";
 
   export let editor: LexicalEditor;
   export let nodeKey: string;
   let selected = false;
   let self: EventTarget;
 
+  function onDelete(event: KeyboardEvent) {
+    if (selected && isNodeSelection(getSelection())) {
+      event.preventDefault();
+      const node = getNodeByKey(nodeKey);
+      if (isHorizontalRuleNode(node)) {
+        node.remove();
+      }
+      setEditorSelection(editor, nodeKey, false);
+    }
+    return false;
+  }
+
   onMount(() => {
     return mergeRegister(
       editor.registerUpdateListener(() => {
         selected = isNodeSelected(editor, nodeKey);
       }),
-      editor.registerCommand(CLICK_COMMAND, (event: MouseEvent) => {
-        if(event.target === self) {
-          if(!event.shiftKey) {
-            clearEditorSelection(editor);
+      editor.registerCommand(
+        CLICK_COMMAND,
+        (event: MouseEvent) => {
+          if (event.target === self) {
+            if (!event.shiftKey) {
+              clearEditorSelection(editor);
+            }
+            setEditorSelection(editor, nodeKey, !selected);
+            return true;
           }
-          setEditorSelection(editor, nodeKey, !selected);
-          return true;
-        }
-        return false; 
-      }, COMMAND_PRIORITY_LOW)
+          return false;
+        },
+        COMMAND_PRIORITY_LOW
+      ),
+      editor.registerCommand(
+        KEY_DELETE_COMMAND,
+        onDelete,
+        COMMAND_PRIORITY_LOW
+      ),
+      editor.registerCommand(
+        KEY_BACKSPACE_COMMAND,
+        onDelete,
+        COMMAND_PRIORITY_LOW
+      )
     );
   });
 </script>
 
-<hr class:selected bind:this={self}/>
+<hr class:selected bind:this={self} />
