08d3a900b35742dcde52ec45cac1cd4d3e49a76e
umaranis <mail@umaranis.com>
2023-01-13 18:02:35 +1100

    refactor: use @lexical/file

--- packages/svelte-lexical/src/components/actionbar/ExportButton.svelte
+++ packages/svelte-lexical/src/components/actionbar/ExportButton.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
-    import {getContext} from 'svelte';
-    import {LexicalEditor} from 'lexical';
-    import {exportFile} from '../../utils/fileImportExport.ts';
+  import {getContext} from 'svelte';
+  import type {LexicalEditor} from 'lexical';
+  import {exportFile} from '@lexical/file';
 
-    const editor: LexicalEditor = getContext('editor');
+  const editor: LexicalEditor = getContext('editor');
 </script>
+
 <button
-        class="action-button export"
-        on:click={() => exportFile(editor, {
-            fileName: `Playground ${new Date().toISOString()}`,
-            source: 'Playground',
-          })
-        }
-        title="Export"
-        aria-label="Export editor state to JSON">
-    <i class="export" />
+  class="action-button export"
+  on:click={() =>
+    exportFile(editor, {
+      fileName: `Playground ${new Date().toISOString()}`,
+      source: 'Playground',
+    })}
+  title="Export"
+  aria-label="Export editor state to JSON">
+  <i class="export" />
 </button>
--- packages/svelte-lexical/src/components/actionbar/ImportButton.svelte
+++ packages/svelte-lexical/src/components/actionbar/ImportButton.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-    import {LexicalEditor} from 'lexical';
-    import {getContext} from 'svelte';
-    import {importFile} from '../../utils/fileImportExport.ts';
+  import type {LexicalEditor} from 'lexical';
+  import {getContext} from 'svelte';
+  import {importFile} from '@lexical/file';
 
-    const editor: LexicalEditor = getContext('editor');
+  const editor: LexicalEditor = getContext('editor');
 </script>
+
 <button
-        class="action-button import"
-        on:click={() => importFile(editor)}
-        title="Import"
-        aria-label="Import editor state from JSON">
-    <i class="import" />
+  class="action-button import"
+  on:click={() => importFile(editor)}
+  title="Import"
+  aria-label="Import editor state from JSON">
+  <i class="import" />
 </button>
