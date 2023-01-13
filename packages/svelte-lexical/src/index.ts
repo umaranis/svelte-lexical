1f97beb49331df93a98641af5162e56fb1c24f8a
umaranis <mail@umaranis.com>
2023-01-13 18:12:41 +1100

    build: generate d.ts for svelte-lexical

--- packages/svelte-lexical/src/index.ts
+++ packages/svelte-lexical/src/index.ts
@@ -1,13 +1,15 @@
-export { default as TextEditor } from './core/TextEditor.svelte';
-export { default as PlainTextPlugin } from './core/plugins/PlainTextPlugin.svelte';
-export { default as RichTextPlugin } from './core/plugins/RichTextPlugin.svelte';
-export { default as HistoryPlugin } from './core/plugins/HistoryPlugin.svelte';
-export { default as ListPlugin } from './core/plugins/ListPlugin.svelte';
+export {default as TextEditor} from './core/TextEditor.svelte';
+export {default as PlainTextPlugin} from './core/plugins/PlainTextPlugin.svelte';
+export {default as RichTextPlugin} from './core/plugins/RichTextPlugin.svelte';
+export {default as HistoryPlugin} from './core/plugins/HistoryPlugin.svelte';
+export {default as ListPlugin} from './core/plugins/ListPlugin.svelte';
 
-export { default as RichTextEditor } from './components/richtext/RichTextEditor.svelte';
-export { default as RichTextComposer } from './components/richtext/RichTextComposer.svelte';
+export {default as RichTextEditor} from './components/richtext/RichTextEditor.svelte';
+export {default as RichTextComposer} from './components/richtext/RichTextComposer.svelte';
 
-export { default as RichTextBasicEditor } from './components/richtext-basic/RichTextBasicEditor.svelte';
-export { default as RichTextBasicComposer } from './components/richtext-basic/RichTextBasicComposer.svelte';
+export {default as RichTextBasicEditor} from './components/richtext-basic/RichTextBasicEditor.svelte';
+export {default as RichTextBasicComposer} from './components/richtext-basic/RichTextBasicComposer.svelte';
 
-export { default as ToolbarBasic } from './components/richtext-basic/ToolbarBasic.svelte';
+export {default as ToolbarBasic} from './components/richtext-basic/ToolbarBasic.svelte';
+
+export {type EditorThemeClasses} from 'lexical';
