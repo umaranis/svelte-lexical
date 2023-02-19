6e60373ba3e905c46c0996db7aed84f00934270c
umaranis <mail@umaranis.com>
2023-02-19 22:29:39 +1100

    bug: yjs already imported, fix #20

--- demos/playground/vite.config.ts
+++ demos/playground/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite';
+import {svelte} from '@sveltejs/vite-plugin-svelte';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      yjs: path.resolve('./node_modules/yjs/src/index.js'),
+    },
+  },
+});
