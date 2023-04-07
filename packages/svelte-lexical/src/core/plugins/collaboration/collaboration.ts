ac0436849112baaa762071e3679cd491416a755e
umaranis <mail@umaranis.com>
2023-04-08 08:37:18 +1000

    fix: ssr error for sveltekit due to collaration.ts (fix #38)

--- packages/svelte-lexical/src/core/plugins/collaboration/collaboration.ts
+++ packages/svelte-lexical/src/core/plugins/collaboration/collaboration.ts
@@ -1,18 +1,18 @@
 import {Doc} from 'yjs';
 import {WebsocketProvider} from 'y-websocket';
 
-const url = new URL(window.location.href);
-const params = new URLSearchParams(url.search);
-const WEBSOCKET_ENDPOINT =
-  params.get('collabEndpoint') || 'ws://localhost:1234';
-const WEBSOCKET_SLUG = 'playground';
-const WEBSOCKET_ID = params.get('collabId') || '0';
-
 // parent dom -> child doc
 export function createWebsocketProvider(
   id: string,
   yjsDocMap: Map<string, Doc>,
 ): WebsocketProvider {
+  const url = new URL(window.location.href);
+  const params = new URLSearchParams(url.search);
+  const WEBSOCKET_ENDPOINT =
+    params.get('collabEndpoint') || 'ws://localhost:1234';
+  const WEBSOCKET_SLUG = 'playground';
+  const WEBSOCKET_ID = params.get('collabId') || '0';
+
   let doc = yjsDocMap.get(id);
 
   if (doc === undefined) {
