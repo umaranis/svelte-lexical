9ec91a1aceae876e17a1afb4571ff61583747f55
umaranis <mail@umaranis.com>
2023-04-21 19:42:32 +1000

    refactor: list indentation simplified

--- packages/svelte-lexical/src/core/plugins/ListPlugin.svelte
+++ packages/svelte-lexical/src/core/plugins/ListPlugin.svelte
@@ -2,21 +2,14 @@
   import {onMount} from 'svelte';
   import {
     $handleListInsertParagraph as handleListInsertParagraph,
-    indentList,
     INSERT_ORDERED_LIST_COMMAND,
     INSERT_UNORDERED_LIST_COMMAND,
     insertList,
-    outdentList,
     REMOVE_LIST_COMMAND,
     removeList,
   } from '@lexical/list';
   import {mergeRegister} from '@lexical/utils';
-  import {
-    COMMAND_PRIORITY_LOW,
-    INDENT_CONTENT_COMMAND,
-    INSERT_PARAGRAPH_COMMAND,
-    OUTDENT_CONTENT_COMMAND,
-  } from 'lexical';
+  import {COMMAND_PRIORITY_LOW, INSERT_PARAGRAPH_COMMAND} from 'lexical';
   import {getEditor} from '../composerContext';
 
   const editor = getEditor();
@@ -24,22 +17,6 @@
   onMount(() => {
     // returns callback to unregister
     return mergeRegister(
-      editor.registerCommand(
-        INDENT_CONTENT_COMMAND,
-        () => {
-          indentList();
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        OUTDENT_CONTENT_COMMAND,
-        () => {
-          outdentList();
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
       editor.registerCommand(
         INSERT_ORDERED_LIST_COMMAND,
         () => {
