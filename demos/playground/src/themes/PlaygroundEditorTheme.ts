f3b72caf32e5fb6e22c85385cc6630916b468016
umaranis <mail@umaranis.com>
2024-03-25 18:02:29 +1100

    minor: table editor css update

--- demos/playground/src/themes/PlaygroundEditorTheme.ts
+++ demos/playground/src/themes/PlaygroundEditorTheme.ts
@@ -61,6 +61,9 @@ const theme: EditorThemeClasses = {
   },
   image: 'editor-image',
   indent: 'PlaygroundEditorTheme__indent',
+  inlineImage: 'inline-editor-image',
+  layoutContainer: 'PlaygroundEditorTheme__layoutContainer',
+  layoutItem: 'PlaygroundEditorTheme__layoutItem',
   link: 'PlaygroundEditorTheme__link',
   list: {
     listitem: 'PlaygroundEditorTheme__listItem',
@@ -99,6 +102,7 @@ const theme: EditorThemeClasses = {
   tableCellSortedIndicator: 'PlaygroundEditorTheme__tableCellSortedIndicator',
   tableResizeRuler: 'PlaygroundEditorTheme__tableCellResizeRuler',
   tableSelected: 'PlaygroundEditorTheme__tableSelected',
+  tableSelection: 'PlaygroundEditorTheme__tableSelection',
   text: {
     bold: 'PlaygroundEditorTheme__textBold',
     code: 'PlaygroundEditorTheme__textCode',
