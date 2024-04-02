8ac9bc0bfdb44e9201739c51138e3d91c192ea06
umaranis <mail@umaranis.com>
2024-04-02 17:55:22 +1100

    build: add route for testing svelte-lexical package

---
+++ packages/svelte-lexical/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to svelte-lexical library</h1>
+<p>
+  Create your package using @sveltejs/package and preview/showcase your work
+  with SvelteKit
+</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
+  to read the documentation
+</p>
