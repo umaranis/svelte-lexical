b943d22744d6176a998baa38ac81b66560566040
laotala828 <5688380+laotala828@users.noreply.github.com>
2024-04-23 13:23:31 -0700

    update

--- demos/playground/svelte.config.js
+++ demos/playground/svelte.config.js
@@ -4,4 +4,11 @@ export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
+  kit: {
+    vite: {
+      ssr: {
+        noExternal: ['prismjs', 'prism-svelte'],
+      },
+    },
+  },
 };
