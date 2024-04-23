b45641587f335d2e75784216d85c4c5bdd0280be
laotala828 <5688380+laotala828@users.noreply.github.com>
2024-04-23 13:12:31 -0700

    update

---
+++ demos/playground/src/MarkdownPlugin.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import {onMount} from 'svelte';
+  import {getEditor} from 'svelte-lexical';
+  import {
+    registerMarkdownShortcuts,
+    BOLD_STAR,
+    HEADING,
+    ORDERED_LIST,
+    UNORDERED_LIST,
+  } from '@lexical/markdown';
+
+  const editor = getEditor();
+
+  onMount(() =>
+    registerMarkdownShortcuts(editor, [
+      BOLD_STAR,
+      HEADING,
+      ORDERED_LIST,
+      UNORDERED_LIST,
+    ]),
+  );
+</script>
