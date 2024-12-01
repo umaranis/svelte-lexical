ed70df881d44dcdc273716822603bf6e9644feb4
umaranis <mail@umaranis.com>
2024-12-01 14:08:19 +1100

    docs: add playground and github links on the first page

--- packages/svelte-lexical/src/routes/+page.svelte
+++ packages/svelte-lexical/src/routes/+page.svelte
@@ -1,17 +1,24 @@
 <script>
   import RichTextComposer from './RichTextComposer.svelte';
   import '../global.css';
+  import GitHubButton from './GitHubButton.svelte';
+  import PlaygroundButton from './PlaygroundButton.svelte';
 </script>
 
 <main>
   <img src="images/logo.svg" alt="Svelte Lexical!" />
   <p>
-    Welcome to <a href="https://github.com/umaranis/svelte-lexical/">
-      svelte-lexical
-    </a>
-    demo built using
-    <a href="https://kit.svelte.dev">SvelteKit</a>
+    Welcome to <span class="emp-sl">Svelte-Lexical</span>
+    , a rich text editor for
+    <span class="emp-svelte">Svelte</span>
+    and
+    <span class="emp-svelte">SvelteKit</span>
+    .
   </p>
+  <div style="margin-top: 5em">
+    <PlaygroundButton />
+    <GitHubButton />
+  </div>
   <div style="text-align: left;">
     <RichTextComposer />
   </div>
@@ -31,5 +38,21 @@
   img {
     margin: 2em;
     max-width: 800px;
+  }
+
+  .emp-sl {
+    font-size: 1em;
+    background-color: #fb8500; /*#ff6600; */
+    color: white;
+    padding: 0.2em 0.2em;
+    border-radius: 0.2em;
+  }
+
+  .emp-svelte {
+    font-size: 1em;
+    background-color: #219ebc;
+    color: white;
+    padding: 0.2em 0.2em;
+    border-radius: 0.2em;
   }
 </style>
