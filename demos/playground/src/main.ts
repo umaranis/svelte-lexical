1d6987a679883772d8b7f765807aa30f6f5e3ff6
umaranis <mail@umaranis.com>
2024-12-23 20:43:14 +1100

    build: playground - fix prettier errors post upgrade

--- demos/playground/src/main.ts
+++ demos/playground/src/main.ts
@@ -2,7 +2,7 @@
 import './index.css';
 import './settings/setupEnv';
 import App from './App.svelte';
-import { mount } from "svelte";
+import {mount} from 'svelte';
 
 const app = mount(App, {
   target: document.getElementById('app')!,
