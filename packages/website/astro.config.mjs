47828e144d4466a5440dc766a5a562c4e2db50f4
umaranis <mail@umaranis.com>
2025-02-26 19:58:38 +1100

    feat: add Svelte integration and svelte-lexical to astro

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -2,28 +2,28 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'svelte-lexical',
-			social: {
-				github: 'https://github.com/umaranis/svelte-lexical',
-			},
-			customCss: ['./src/styles/custom.css'],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'svelte-lexical',
+        social: {
+            github: 'https://github.com/umaranis/svelte-lexical',
+        },
+        customCss: ['./src/styles/custom.css'],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), svelte()],
 });
