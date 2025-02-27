7e3115d906271b7cb9eed9a1571c5f6434384646
umaranis <mail@umaranis.com>
2025-02-27 17:24:11 +1100

    refactor: use registerList function

--- packages/svelte-lexical/src/lib/core/plugins/ListPlugin.svelte
+++ packages/svelte-lexical/src/lib/core/plugins/ListPlugin.svelte
@@ -1,59 +1,12 @@
 <script lang="ts">
   import {onMount} from 'svelte';
-  import {
-    $handleListInsertParagraph as handleListInsertParagraph,
-    INSERT_ORDERED_LIST_COMMAND,
-    INSERT_UNORDERED_LIST_COMMAND,
-    insertList,
-    REMOVE_LIST_COMMAND,
-    removeList,
-  } from '@lexical/list';
-  import {mergeRegister} from '@lexical/utils';
-  import {COMMAND_PRIORITY_LOW, INSERT_PARAGRAPH_COMMAND} from 'lexical';
+  import {registerList} from '@lexical/list';
   import {getEditor} from '../composerContext.js';
 
   const editor = getEditor();
 
   onMount(() => {
     // returns callback to unregister
-    return mergeRegister(
-      editor.registerCommand(
-        INSERT_ORDERED_LIST_COMMAND,
-        () => {
-          insertList(editor, 'number');
-          return true;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        INSERT_UNORDERED_LIST_COMMAND,
-        () => {
-          insertList(editor, 'bullet');
-          return true;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        REMOVE_LIST_COMMAND,
-        () => {
-          removeList(editor);
-          return true;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        INSERT_PARAGRAPH_COMMAND,
-        () => {
-          const hasHandledInsertParagraph = handleListInsertParagraph();
-
-          if (hasHandledInsertParagraph) {
-            return true;
-          }
-
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-    );
+    return registerList(editor);
   });
 </script>
