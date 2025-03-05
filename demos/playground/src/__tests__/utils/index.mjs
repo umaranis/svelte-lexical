32c47dfce7dd182d98e008ec4c9dc9c392ac4d56
umaranis <mail@umaranis.com>
2025-03-05 20:37:44 +1100

    test: add keyboard shortcuts for deleting lines on Mac

--- demos/playground/src/__tests__/utils/index.mjs
+++ demos/playground/src/__tests__/utils/index.mjs
@@ -222,7 +222,7 @@ async function assertHTMLOnPageOrFrame(
       ignoreInlineStyles,
     });
 
-    actual = actualHtmlModificationsCallback(actual);
+    actual = await actualHtmlModificationsCallback(actual);
 
     expect(
       actual,
