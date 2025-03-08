acb9ccf4da7982d36c97d702af95e030f0fcefe4
umaranis <mail@umaranis.com>
2025-03-08 19:52:45 +1100

    docs: getting started guide

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,29 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'svelte-lexical',
-			social: {
-				github: 'https://github.com/umaranis/svelte-lexical',
-			},
-			customCss: ['./src/styles/custom.css'],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'svelte-lexical',
+      social: {
+        github: 'https://github.com/umaranis/svelte-lexical',
+      },
+      customCss: ['./src/styles/custom.css'],
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {label: 'Getting Started', slug: 'guides/getting-started'},
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: {directory: 'reference'},
+        },
+      ],
+    }),
+  ],
 });
