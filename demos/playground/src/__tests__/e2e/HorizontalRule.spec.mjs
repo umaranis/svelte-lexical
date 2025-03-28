f6799368c1f2aaa93dfa670f808a3a4292d9ca4e
umaranis <mail@umaranis.com>
2025-03-29 08:39:45 +1100

    test: enhance HorizontalRule tests to validate behavior with ListItemNode and clipboard operations

--- demos/playground/src/__tests__/e2e/HorizontalRule.spec.mjs
+++ demos/playground/src/__tests__/e2e/HorizontalRule.spec.mjs
@@ -15,6 +15,7 @@ import {
 import {
   assertHTML,
   assertSelection,
+  click,
   copyToClipboard,
   focusEditor,
   html,
@@ -23,7 +24,13 @@ import {
   selectFromInsertDropdown,
   test,
   waitForSelector,
+  withExclusiveClipboardAccess,
 } from '../utils/index.mjs';
+
+async function toggleBulletList(page) {
+  await click(page, '.block-controls');
+  await click(page, '.dropdown .icon.bullet-list');
+}
 
 test.describe('HorizontalRule', () => {
   test.beforeEach(({isCollab, page}) => initialize({isCollab, page}));
@@ -278,6 +285,142 @@ test.describe('HorizontalRule', () => {
     });
   });
 
+  test('Will add a horizontal rule and split a TextNode across 2 ListItemNode if the caret is in the middle of the TextNode, moving selection to the start of the new ParagraphNode', async ({
+    page,
+    isPlainText,
+  }) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+    await toggleBulletList(page);
+
+    await page.keyboard.type('Test');
+
+    await assertSelection(page, {
+      anchorOffset: 4,
+      anchorPath: [0, 0, 0, 0],
+      focusOffset: 4,
+      focusPath: [0, 0, 0, 0],
+    });
+
+    await assertHTML(
+      page,
+      html`
+        <ul class="PlaygroundEditorTheme__ul">
+          <li
+            class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__ltr"
+            dir="ltr"
+            value="1">
+            <span data-lexical-text="true">Test</span>
+          </li>
+        </ul>
+      `,
+    );
+
+    await moveLeft(page, 2);
+
+    await assertSelection(page, {
+      anchorOffset: 2,
+      anchorPath: [0, 0, 0, 0],
+      focusOffset: 2,
+      focusPath: [0, 0, 0, 0],
+    });
+
+    await selectFromInsertDropdown(page, '.horizontal-rule');
+
+    await waitForSelector(page, 'hr');
+
+    await assertHTML(
+      page,
+      html`
+        <ul class="PlaygroundEditorTheme__ul">
+          <li
+            class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__ltr"
+            dir="ltr"
+            value="1">
+            <span data-lexical-text="true">Te</span>
+          </li>
+        </ul>
+        <hr
+          class="PlaygroundEditorTheme__hr"
+          contenteditable="false"
+          data-lexical-decorator="true" />
+        <ul class="PlaygroundEditorTheme__ul">
+          <li
+            class="PlaygroundEditorTheme__listItem PlaygroundEditorTheme__ltr"
+            dir="ltr"
+            value="1">
+            <span data-lexical-text="true">st</span>
+          </li>
+        </ul>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 0,
+      anchorPath: [2, 0, 0, 0],
+      focusOffset: 0,
+      focusPath: [2, 0, 0, 0],
+    });
+  });
+
+  test('Will add a horizontal rule and split a TextNode across 2 ListItemNode if the caret is in an empty ListItemNode, moving selection to the start of the new ListItemNode (#6849)', async ({
+    page,
+    isPlainText,
+  }) => {
+    test.skip(isPlainText);
+    await focusEditor(page);
+    await toggleBulletList(page);
+
+    await assertSelection(page, {
+      anchorOffset: 0,
+      anchorPath: [0, 0],
+      focusOffset: 0,
+      focusPath: [0, 0],
+    });
+
+    await assertHTML(
+      page,
+      html`
+        <ul class="PlaygroundEditorTheme__ul">
+          <li class="PlaygroundEditorTheme__listItem" value="1">
+            <br />
+          </li>
+        </ul>
+      `,
+    );
+
+    await selectFromInsertDropdown(page, '.horizontal-rule');
+
+    await waitForSelector(page, 'hr');
+
+    await assertHTML(
+      page,
+      html`
+        <ul class="PlaygroundEditorTheme__ul">
+          <li class="PlaygroundEditorTheme__listItem" value="1">
+            <br />
+          </li>
+        </ul>
+        <hr
+          class="PlaygroundEditorTheme__hr"
+          contenteditable="false"
+          data-lexical-decorator="true" />
+        <ul class="PlaygroundEditorTheme__ul">
+          <li class="PlaygroundEditorTheme__listItem" value="1">
+            <br />
+          </li>
+        </ul>
+      `,
+    );
+
+    await assertSelection(page, {
+      anchorOffset: 0,
+      anchorPath: [2, 0],
+      focusOffset: 0,
+      focusPath: [2, 0],
+    });
+  });
+
   test('Can copy and paste a horizontal rule', async ({
     page,
     isPlainText,
@@ -314,37 +457,39 @@ test.describe('HorizontalRule', () => {
     // Select all the text
     await selectAll(page);
 
-    // Copy all the text
-    const clipboard = await copyToClipboard(page);
+    await withExclusiveClipboardAccess(async () => {
+      // Copy all the text
+      const clipboard = await copyToClipboard(page);
 
-    // Delete content
-    await page.keyboard.press('Backspace');
+      // Delete content
+      await page.keyboard.press('Backspace');
 
-    await pasteFromClipboard(page, clipboard);
+      await pasteFromClipboard(page, clipboard);
 
-    await assertHTML(
-      page,
-      html`
-        <p class="PlaygroundEditorTheme__paragraph"><br /></p>
-        <hr
-          class="PlaygroundEditorTheme__hr"
-          contenteditable="false"
-          data-lexical-decorator="true" />
-        <p class="PlaygroundEditorTheme__paragraph"><br /></p>
-      `,
-    );
+      await assertHTML(
+        page,
+        html`
+          <p class="PlaygroundEditorTheme__paragraph"><br /></p>
+          <hr
+            class="PlaygroundEditorTheme__hr"
+            contenteditable="false"
+            data-lexical-decorator="true" />
+          <p class="PlaygroundEditorTheme__paragraph"><br /></p>
+        `,
+      );
 
-    await assertSelection(page, {
-      anchorOffset: 0,
-      anchorPath: [2],
-      focusOffset: 0,
-      focusPath: [2],
+      await assertSelection(page, {
+        anchorOffset: 0,
+        anchorPath: [2],
+        focusOffset: 0,
+        focusPath: [2],
+      });
+
+      await page.keyboard.press('ArrowUp');
+      await page.keyboard.press('Backspace');
+
+      await pasteFromClipboard(page, clipboard);
     });
-
-    await page.keyboard.press('ArrowUp');
-    await page.keyboard.press('Backspace');
-
-    await pasteFromClipboard(page, clipboard);
 
     await assertHTML(
       page,
