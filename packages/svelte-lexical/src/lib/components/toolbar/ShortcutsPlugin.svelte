8c32c92b924fb9690e9d5dc930aee23bdc3f6dce
umaranis <mail@umaranis.com>
2025-06-03 08:10:13 +1000

    refactor: deprecate KEY_MODIFIER_COMMAND and use KEY_DOWN_COMMAND

--- packages/svelte-lexical/src/lib/components/toolbar/ShortcutsPlugin.svelte
+++ packages/svelte-lexical/src/lib/components/toolbar/ShortcutsPlugin.svelte
@@ -11,7 +11,8 @@
     FORMAT_ELEMENT_COMMAND,
     FORMAT_TEXT_COMMAND,
     INDENT_CONTENT_COMMAND,
-    KEY_MODIFIER_COMMAND,
+    isModifierMatch,
+    KEY_DOWN_COMMAND,
     OUTDENT_CONTENT_COMMAND,
   } from 'lexical';
 
@@ -59,81 +60,66 @@
   const isLink = getIsLink();
   const fontSize = getFontSize();
 
-  const keyboardShortcutsHandler = (payload: KeyboardEvent) => {
-    const event: KeyboardEvent = payload;
-
-    if (isFormatParagraph(event)) {
-      event.preventDefault();
+  const keyboardShortcutsHandler = (event: KeyboardEvent) => {
+    // Short-circuit, a least one modifier must be set
+    if (isModifierMatch(event, {})) {
+      return false;
+    } else if (isFormatParagraph(event)) {
       formatParagraph($activeEditor);
     } else if (isFormatHeading(event)) {
-      event.preventDefault();
       const {code} = event;
       const headingSize = `h${code[code.length - 1]}` as HeadingTagType;
       formatHeading($activeEditor, $blockType, headingSize);
     } else if (isFormatBulletList(event)) {
-      event.preventDefault();
       formatBulletList($activeEditor, $blockType);
     } else if (isFormatNumberedList(event)) {
-      event.preventDefault();
       formatNumberedList($activeEditor, $blockType);
     } else if (isFormatCheckList(event)) {
-      event.preventDefault();
       formatCheckList($activeEditor, $blockType);
     } else if (isFormatCode(event)) {
-      event.preventDefault();
       formatCode($activeEditor, $blockType);
     } else if (isFormatQuote(event)) {
-      event.preventDefault();
       formatQuote($activeEditor, $blockType);
     } else if (isStrikeThrough(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough');
     } else if (isIndent(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(INDENT_CONTENT_COMMAND, undefined);
     } else if (isOutdent(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(OUTDENT_CONTENT_COMMAND, undefined);
     } else if (isCenterAlign(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'center');
     } else if (isLeftAlign(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'left');
     } else if (isRightAlign(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'right');
     } else if (isJustifyAlign(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_ELEMENT_COMMAND, 'justify');
     } else if (isSubscript(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'subscript');
     } else if (isSuperscript(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'superscript');
     } else if (isInsertCodeBlock(event)) {
-      event.preventDefault();
       $activeEditor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code');
     } else if (isIncreaseFontSize(event)) {
-      event.preventDefault();
       increaseFontSize($activeEditor, Number($fontSize.slice(0, -2)));
     } else if (isDecreaseFontSize(event)) {
-      event.preventDefault();
       decreaseFontSize($activeEditor, Number($fontSize.slice(0, -2)));
     } else if (isClearFormatting(event)) {
-      event.preventDefault();
       clearFormatting($activeEditor);
     } else if (isInsertLink(event)) {
       InsertLink($activeEditor, $isLink);
+    } else {
+      // No match for any of the event handlers
+      return false;
     }
 
-    return false;
+    event.preventDefault();
+    return true;
   };
 
   $effect(() => {
     return $activeEditor.registerCommand(
-      KEY_MODIFIER_COMMAND,
+      KEY_DOWN_COMMAND,
       keyboardShortcutsHandler,
       COMMAND_PRIORITY_NORMAL,
     );
