c0001d986e7ecdb4902f104f096aff1ce1d26c3e
Derek Rush <drush81@gmail.com>
2025-10-23 17:52:36 -0700

    fixes

--- packages/svelte-lexical/src/lib/components/generic/button/CloseCircleButton.svelte
+++ packages/svelte-lexical/src/lib/components/generic/button/CloseCircleButton.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
-  let { onclick = () => {} }: { onclick?: (event: MouseEvent) => void } = $props();
+  let {onclick = () => {}}: {onclick?: (event: MouseEvent) => void} = $props();
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<svg { onclick } class="close-icon" viewBox="0 0 24 24" fill="currentColor" style="cursor: pointer;">
-  <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+<svg
+  {onclick}
+  class="close-icon"
+  viewBox="0 0 24 24"
+  fill="currentColor"
+  style="cursor: pointer;">
+  <path
+    d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
 </svg>
-
