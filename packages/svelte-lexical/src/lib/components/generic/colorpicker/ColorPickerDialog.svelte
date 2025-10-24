01e432d3853dcae049c24774497b03686e8de581
Derek Rush <drush81@gmail.com>
2025-10-23 17:42:39 -0700

    fix

--- packages/svelte-lexical/src/lib/components/generic/colorpicker/ColorPickerDialog.svelte
+++ packages/svelte-lexical/src/lib/components/generic/colorpicker/ColorPickerDialog.svelte
@@ -42,7 +42,7 @@
 </script>
 
 <ModalDialog bind:showModal>
-  <CloseCircleButton on:click={close} />
+  <CloseCircleButton onclick={close} />
 
   <div class="modal">
     <h2 class="Modal__title">{title}</h2>
