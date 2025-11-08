e74a7adb1e74899868f5206435724796090b17de
Syed Umar Anis <umaranis@users.noreply.github.com>
2025-11-08 13:09:46 +1100

    feat: context menu component

---
+++ packages/svelte-lexical/src/lib/components/generic/contextmenu/contextMenuHelpers.ts
@@ -0,0 +1,134 @@
+import {
+  $getSelection,
+  $isRangeSelection,
+  createCommand,
+  TextNode,
+  type LexicalCommand,
+} from 'lexical';
+import type {Snippet} from 'svelte';
+
+export const SCROLL_TYPEAHEAD_OPTION_INTO_VIEW_COMMAND: LexicalCommand<{
+  index: number;
+  option: MenuOption;
+}> = createCommand('SCROLL_TYPEAHEAD_OPTION_INTO_VIEW_COMMAND');
+
+export type MenuTextMatch = {
+  leadOffset: number;
+  matchingString: string;
+  replaceableString: string;
+};
+
+export type MenuResolution = {
+  match?: MenuTextMatch;
+  getRect: () => DOMRect;
+};
+
+export class MenuOption {
+  key: string;
+  ref?: HTMLElement | null;
+
+  constructor(key: string) {
+    this.key = key;
+    this.ref = null;
+    this.setRefElement = this.setRefElement.bind(this);
+  }
+
+  setRefElement(element: HTMLElement | null) {
+    this.ref = element;
+  }
+}
+
+export type MenuRenderFn<TOption extends MenuOption> = Snippet<
+  [
+    anchorElementRef: HTMLElement | null,
+    itemProps: {
+      selectedIndex: number | null;
+      selectOptionAndCleanUp: (option: TOption) => void;
+      setHighlightedIndex: (index: number) => void;
+      options: Array<TOption>;
+    },
+    matchingString: string | null,
+  ]
+>;
+
+export const scrollIntoViewIfNeeded = (target: HTMLElement) => {
+  const typeaheadContainerNode = document.getElementById('typeahead-menu');
+  if (!typeaheadContainerNode) {
+    return;
+  }
+
+  const typeaheadRect = typeaheadContainerNode.getBoundingClientRect();
+
+  if (typeaheadRect.top + typeaheadRect.height > window.innerHeight) {
+    typeaheadContainerNode.scrollIntoView({
+      block: 'center',
+    });
+  }
+
+  if (typeaheadRect.top < 0) {
+    typeaheadContainerNode.scrollIntoView({
+      block: 'center',
+    });
+  }
+
+  target.scrollIntoView({block: 'nearest'});
+};
+
+/**
+ * Walk backwards along user input and forward through entity title to try
+ * and replace more of the user's text with entity.
+ */
+function getFullMatchOffset(
+  documentText: string,
+  entryText: string,
+  offset: number,
+): number {
+  let triggerOffset = offset;
+  for (let i = triggerOffset; i <= entryText.length; i++) {
+    if (documentText.slice(-i) === entryText.substring(0, i)) {
+      triggerOffset = i;
+    }
+  }
+  return triggerOffset;
+}
+
+/**
+ * Split Lexical TextNode and return a new TextNode only containing matched text.
+ * Common use cases include: removing the node, replacing with a new node.
+ */
+export function $splitNodeContainingQuery(
+  match: MenuTextMatch,
+): TextNode | null {
+  const selection = $getSelection();
+  if (!$isRangeSelection(selection) || !selection.isCollapsed()) {
+    return null;
+  }
+  const anchor = selection.anchor;
+  if (anchor.type !== 'text') {
+    return null;
+  }
+  const anchorNode = anchor.getNode();
+  if (!anchorNode.isSimpleText()) {
+    return null;
+  }
+  const selectionOffset = anchor.offset;
+  const textContent = anchorNode.getTextContent().slice(0, selectionOffset);
+  const characterOffset = match.replaceableString.length;
+  const queryOffset = getFullMatchOffset(
+    textContent,
+    match.matchingString,
+    characterOffset,
+  );
+  const startOffset = selectionOffset - queryOffset;
+  if (startOffset < 0) {
+    return null;
+  }
+  let newNode;
+  if (startOffset === 0) {
+    [newNode] = anchorNode.splitText(selectionOffset);
+  } else {
+    [, newNode] = anchorNode.splitText(startOffset, selectionOffset);
+  }
+
+  return newNode;
+}
